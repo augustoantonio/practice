489eaf377dd4be3a53cab05c92b69580ec09edb7
augusto <augustoanton@gmail.com>
2014-03-12 22:01:43 +0100

    phpnet objects

--- public/objects.php
+++ public/objects.php
@@ -1,1 +1,12 @@
 <?php
+$vacio = "";
+$colores = array ('azul', 'verde', 'rosa');
+$verano = array ('cielo' => 'azul', 'comida' => 'gazpacho', 'temperatura' => 28, 'mola' => true);
+$objeto = (object) $vacio;
+echo '<pre>';
+var_dump($objeto);
+echo '</pre>';
+foreach ($objeto as $key => $value){
+	$a[$key] = $value;
+}
+print_r($a);
